e77f2ece4f8016f03f6b92ef245afd392d4d04db
xiraynedev <xiraynedev@gmail.com>
2022-07-14 21:14:28 -0400

    re-introduce testing error

--- src/attack.test.js
+++ src/attack.test.js
@@ -1,9 +1,7 @@
 const attack = require('./attack');
-const criticalAttack = require('./criticalAttack');
 
 test('should return damage sentence', () => {
   const result = attack('Ogre', 25);
-  const criticalResult = criticalAttack('Ogre', 25);
 
   expect(result).toBe('Ogre dealt 21 damage!');
 });
