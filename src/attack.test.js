7f0cf49af5016f474f0d3b5f424b19f36bf144a0
xiraynedev <xiraynedev@gmail.com>
2022-07-14 21:13:10 -0400

    introduce testing error

--- src/attack.test.js
+++ src/attack.test.js
@@ -1,6 +1,9 @@
 const attack = require('./attack');
+const criticalAttack = require('./criticalAttack');
 
 test('should return damage sentence', () => {
   const result = attack('Ogre', 25);
-  expect(result).toBe('Ogre dealt 25 damage!');
+  const criticalResult = criticalAttack('Ogre', 25);
+
+  expect(result).toBe('Ogre dealt 21 damage!');
 });
