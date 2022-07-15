6bca46e62a7b80d0e705eff1b89b07ade8f0fceb
xiraynedev <xiraynedev@gmail.com>
2022-07-15 12:48:05 -0400

    lock in node version and refactor code

--- src/attack.test.js
+++ src/attack.test.js
@@ -3,5 +3,5 @@ const attack = require('./attack');
 test('should return damage sentence', () => {
   const result = attack('Ogre', 25);
 
-  expect(result).toBe('Ogre dealt 21 damage!');
+  expect(result).toBe('Ogre dealt 25 damage!');
 });
